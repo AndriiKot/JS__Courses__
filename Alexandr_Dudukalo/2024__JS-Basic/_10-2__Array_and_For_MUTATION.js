93d960b65e8dfa44ab4a2098378c6d60b744f530
AndriiKot <a_kocyuba@ukr.net>
2024-07-30 13:53:49 +0200

    LAERN JS 'Alexandr Dudukalo' '! Урок 1 Array MUTATION !

---
+++ Alexandr_Dudukalo/2024__JS-Basic/_10-2__Array_and_For_MUTATION.js
@@ -0,0 +1,24 @@
+﻿const menu = ["Menu", "Home", "About", "Contact"];
+
+// !!! WARNING !!!
+console.log(menu);
+// [ 'Menu', 'Home', 'About', 'Contact' ]
+
+// for (let item of menu) {
+//   item = item + "!!!";
+//   console.log(item); //
+// }
+
+for (let i = 0; i < menu.length; i++) {
+  menu[i] += "!!!";                       // !!! MUTATION !!!
+  console.log(menu[i]);
+}
+
+// Menu!!!
+// Home!!!
+// About!!!
+// Contact!!!
+
+console.log(menu);
+// [ 'Menu!!!', 'Home!!!', 'About!!!', 'Contact!!!' ]
+// !!! WARNING !!!
